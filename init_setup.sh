0d8d88e37659124aff3db3d575ba0bf6a2ff4ae6
KadamSujit <sujitpkadam@gmail.com>
2023-05-10 21:08:48 +0530

    init setup done2

--- init_setup.sh
+++ init_setup.sh
@@ -4,5 +4,5 @@ conda create --prefix ./env python=3.8 -y
 echo [$(date)] : "activating the environment"
 source activate ./env
 echo [$(date)] : "installing the dev requirements"
-pip install -r requirements_dev.txt
+pip install -r requirements_dev.txt --user
 echo [$(date)] : "END"
